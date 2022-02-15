dc77e43e2d8d4f4783df946e0018de6614d9433e
nilson <nilson.dyrmishi@gmail.com>
2022-02-14 21:43:44 -0500

    commit

--- index.ts
+++ index.ts
@@ -6,64 +6,158 @@ const cors = require("cors");
 
 type Quote ={
     id:number
-    author:string
     theQuote:string
+    firstName:string
+    lastName:string
+    image:string
+    age:number
 }
 
 const quotes : Quote[]=[
     {
-        id: 1,
-        theQuote: "To be or not to be",
-        author: "William Sheksprie"
-    },
-    {
-        id: 2,
+        id: Math.random(),
         theQuote: "Memento Mori",
-        author: "Marcus Aurelius"
+        firstName:"Marcus",
+        lastName:"Aurelius",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg/800px-L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg",
+        age:53
     },
     {
-        id: 3,
+        id: Math.random(),
+        theQuote: "Memento Mori",
+        firstName:"Marcus",
+        lastName:"Aurelius",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg/800px-L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg",
+        age:53
+
+    },
+    {
+        id: Math.random(),
         theQuote: "i came,I saw,I conquered",
-        author: "Caesar"
+        firstName:"",
+        lastName:"Caesar",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg/800px-L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg",
+        age:53
+
     },
     {
-        id: 4,
-        theQuote: "To be or not to be",
-        author: "William Shakespeare"
+        id: Math.random(),
+        theQuote: "Memento Mori",
+        firstName:"Marcus",
+        lastName:"Aurelius",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg/800px-L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg",
+        age:53
+
+
     },
     {
-        id: 5,
-        theQuote: "To be or not to be",
-        author: "William Shakespeare"
+        id: Math.random(),
+        theQuote: "Memento Mori",
+        firstName:"Marcus",
+        lastName:"Aurelius",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg/800px-L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg",
+        age:53
+
+
     },
     {
-        id: 6,
-        theQuote: "To be or not to be",
-        author: "William Shakespeare"
-    }, {
-        id: 7,
-        theQuote: "To be or not to be",
-        author: "William Shakespeare"
+        id: Math.random(),
+        theQuote: "Memento Mori",
+        firstName:"Marcus",
+        lastName:"Aurelius",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg/800px-L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg",
+        age:53
+
+
     },
     {
-        id: 8,
-        theQuote: "To be or not to be",
-        author: "William Shakespeare"
+        id: Math.random(),
+        theQuote: "Memento Mori",
+        firstName:"Marcus",
+        lastName:"Aurelius",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg/800px-L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg",
+        age:53
+
+
+    },
+    {
+        id: Math.random(),
+        theQuote: "Memento Mori",
+        firstName:"Marcus",
+        lastName:"Aurelius",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg/800px-L%27Image_et_le_Pouvoir_-_Buste_cuirass%C3%A9_de_Marc_Aur%C3%A8le_ag%C3%A9_-_2.jpg",
+        age:53
+
+
     },
 
 ]
+
+app.use(express.json());
 app.use(
     cors({
         origin: "*"
     })
 );
+
 app.get('/',function (req,res){
     res.send('We are learning NODE JSSS')
 })
 
+app.get("/random", function (req, res) {
+    const randomQuote = Math.floor(Math.random() * quotes.length);
+    res.send(quotes[randomQuote]);
+});
+
 app.get('/quotes',function (req,res){
     res.send(quotes)
 })
+
+app.get("/quotes/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const match = quotes.find((quote) => quote.id === id);
+    if (match) {
+        res.send(match);
+    } else {
+        res.status(404).send({ error: "Sorry :/." });
+    }
+});
+app.post("/quotes", (req, res) => {
+    const { theQuote, firstName, lastName, age, image } = req.body;
+    const errors = [];
+    if (typeof theQuote !== "string") {
+        errors.push("the Quote is missing or not a string");
+    }
+    if (typeof firstName !== "string") {
+        errors.push("First name is missing or not a string");
+    }
+    if (typeof lastName !== "string") {
+        errors.push("Last name is missing or not a string");
+    }
+    if (typeof age !== "number" && age < 0) {
+        errors.push("Age should be a number higher than 0!");
+    }
+    if (typeof image !== "string") {
+        errors.push("Image is missing or not a string");
+    }
+
+    if (errors.length === 0) {
+        const newQuote: Quote = {
+            id: Math.random(),
+            theQuote: theQuote,
+            firstName: firstName,
+            lastName: lastName,
+            age: age,
+            image: image
+        };
+        quotes.unshift(newQuote);
+        res.status(201).send(newQuote);
+    } else {
+        res.status(400).send({ errors: errors }
+        );
+    }
+}
+);
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
